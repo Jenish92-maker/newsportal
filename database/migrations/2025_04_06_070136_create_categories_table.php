5c993ba96f4f2df54b68cc3f27f8ddbd169512ee
Jenish92--maker <jenishshah277@gmail.com>
2025-04-28 10:21:41 +0545

    Second-commit

---
+++ database/migrations/2025_04_06_070136_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nep_title');
+            $table->string('Eng_title');
+            $table->string('slug');
+            $table->longtext('meta_keywords')->nullable();
+            $table->longtext('meta_description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
